f43a0905125e3aee67e804cb5cc8da7d91b826eb
bfehr11 <89813070+bfehr11@users.noreply.github.com>
2025-11-14 19:52:31 +0000

    added download of other data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
+import json
+import plotly.express as px
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+data = json.load(open('bls_unemployment.json'))
+data = pd.DataFrame(data['Results']['series'][0]['data'])
+data['periodName'] = data['periodName'] + " " + data['year']
+data = data[::-1]
+
+fig = px.line(data, x="periodName", y="value", title="The Unemployment Rate over the Years"
+              , labels={'periodName': "", 'value': "Unemployment Rate"})
+fig.show()
